8a6595be74608e35ac4708568b9f163583f5e312
HakimYu <yhjredin@gmail.com>
2024-09-06 22:51:40 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,51 @@
+import {createApp} from "vue";
+import App from "./App.vue";
+
+// Vuetify
+import 'vuetify/styles'
+import {createVuetify} from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+//pinia
+import { createPinia } from 'pinia'
+import {storeF} from './store'
+const pinia = createPinia()
+const app = createApp({})
+app.use(pinia);
+const settings = storeF(pinia);
+const lightTheme = {
+    dark: false,
+    colors: {
+        primary: settings.color.light.primary,
+        secondary: settings.color.light.secondary,
+        background: settings.color.light.background,
+    }
+};
+const darkTheme = {
+    dark: false,
+    colors: {
+        primary: settings.color.dark.primary,
+        secondary: settings.color.dark.secondary,
+        background: settings.color.dark.background,
+    }
+};
+const vuetify = createVuetify({
+    components,
+    directives,
+    theme: {
+        defaultTheme: 'light',
+        variations: {
+            colors: ['primary', 'secondary', 'background'],
+            lighten: 5,
+            darken: 5,
+        },
+        themes: {
+            light: lightTheme,
+            dark: darkTheme
+        }
+    },
+})
+
+createApp(App).use(vuetify).use(pinia).mount("#app");
---
+++ src/store.ts
@@ -0,0 +1,25 @@
+import { createPinia,defineStore } from 'pinia'
+import {ref} from "vue";
+import persisitedstate from "pinia-plugin-persistedstate";
+
+const pinia = createPinia()
+pinia.use(persisitedstate);
+export const storeF = defineStore('store', () => {
+    const color = ref({
+        'light': {
+            primary: '#ffffff',
+            secondary: '#ffffff',
+            background: '#ffffff'
+        },
+        'dark': {
+            primary: '#212121',
+            secondary: '#212121',
+            background: '#121212'
+        }
+    });
+
+    return {
+        color
+    }
+})
+export default pinia
